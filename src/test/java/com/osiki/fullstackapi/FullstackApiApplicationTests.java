8afe4f084d1327784ed3c58d7da2cd4fec779db6
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 16:02:11 +0100

    first commit

---
+++ src/test/java/com/osiki/fullstackapi/FullstackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.fullstackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
