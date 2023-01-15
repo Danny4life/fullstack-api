e6e06d9988c31e9b59d2dcfe3a08b9e65dcb075f
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 21:29:33 +0100

    extends jpa repository interface

---
+++ src/main/java/com/osiki/fullstackapi/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.osiki.fullstackapi.repository;
+
+import com.osiki.fullstackapi.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
