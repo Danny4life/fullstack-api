126b82de002e23763ff3a0ad33da086a0fc181ac
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 21:32:54 +0100

    create Employee controller class

---
+++ src/main/java/com/osiki/fullstackapi/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.osiki.fullstackapi.controller;
+
+import com.osiki.fullstackapi.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
